2e840cab9bb9d2b84827d858b577ef73c6e5c07c
homuhomu <gyudouhu3208@gmail.com>
2021-09-18 10:35:13 +0900

    Nuxt.jsのgithub用の設定の追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,18 @@
 import colors from 'vuetify/es5/util/colors'
+import penv from './env/production.js'
+import lenv from './env/local.js'
+const environment = process.env.NODE_ENV || 'local'
+console.log(environment)
+const env = () => {
+  if (process.env.NODE_ENV == 'production') {
+    return penv
+  } else {
+    return lenv
+  }
+}
 
 export default {
+  base: env().BASE_URL,
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
@@ -15,20 +27,22 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: `${env().BASE_URL}/favicon.ico`,
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -52,8 +66,8 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'jp',
+    },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
@@ -69,13 +83,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
