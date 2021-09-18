722dd3ea0df41c715ad9ddcece125799e6740d27
homuhomu <gyudouhu3208@gmail.com>
2021-09-18 10:15:46 +0900

    初回

---
+++ components/colsInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-col
+    :cols="cols"
+    :lg="lg"
+    :md="md"
+    :sm="sm"
+    style="border: dotted 1px"
+    name="div"
+  >
+    <v-row>
+      <v-col cols="12">
+        <h2>要素{{ counter }}</h2>
+      </v-col>
+      <v-col cols="12">
+        <span
+          ><v-text-field v-model.number="lg" label="lg" type="number" outlined
+        /></span>
+        <span
+          ><v-text-field v-model.number="md" label="md" type="number" outlined
+        /></span>
+        <span
+          ><v-text-field v-model.number="sm" label="sm" type="number" outlined
+        /></span>
+        <span
+          ><v-text-field
+            v-model.number="cols"
+            label="cols"
+            type="number"
+            outlined
+        /></span>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    icols: { type: Number, default: 0 },
+    ilg: { type: Number, default: 0 },
+    imd: { type: Number, default: 0 },
+    ism: { type: Number, default: 0 },
+    index: { type: Number, default: 0 },
+  },
+  data() {
+    const cols = this.icols
+    const md = this.imd
+    const lg = this.ilg
+    const sm = this.ism
+    return {
+      cols,
+      lg,
+      md,
+      sm,
+    }
+  },
+  computed: {
+    counter() {
+      const data = '' + (this.index + 1)
+      return data.padStart(2, '0')
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-cols cols="12">
+        <h1>colsの指定サンプルページ</h1>
+      </v-cols>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-select
+          v-model="count"
+          :items="pullDownList"
+          label="個数を選択"
+          @change="listReset"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <cols-input
+        v-for="(item, index) in list"
+        :key="index"
+        :icols="item.cols"
+        :ilg="item.lg"
+        :imd="item.md"
+        :ism="item.sm"
+        :index="index"
+      ></cols-input>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import colsInput from '~/components/colsInput.vue'
+const data = { cols: 1, lg: 1, md: 1, sm: 1 }
+export default {
+  components: { colsInput },
+  data() {
+    const list = []
+    const count = 12
+    const pullDownList = []
+    for (let i = 0; i < count; i++) {
+      list.push(Object.assign({}, data))
+      pullDownList.push(i + 1)
+    }
+    return {
+      list,
+      count,
+      pullDownList,
+    }
+  },
+  mounted() {},
+  methods: {
+    listReset() {
+      this.list.length = 0
+      for (let i = 0; i < this.count; i++) {
+        this.list.push(Object.assign({}, data))
+      }
+    },
+  },
+}
+</script>
