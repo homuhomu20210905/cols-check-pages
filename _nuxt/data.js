db5e709c8456d9f0e7b9ecb7c2396a2cd68020e5
homuhomu20210905 <homuhomu20210905@users.noreply.github.com>
2021-09-22 11:45:05 +0000

    deploy: 010f797cc6810641c214a738e5d90904c84f50a4

---
+++ _nuxt/data.js
@@ -0,0 +1,16 @@
+export default {
+  columns() {
+    return { cols: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }
+  },
+  setColumns(a, b) {
+    a.cols = b.cols
+    a.xl = b.xl
+    a.lg = b.lg
+    a.md = b.md
+    a.sm = b.sm
+    a.xs = b.xs
+    return a
+  },
+  MAX_COUNT: 12,
+  NOMAL_COUNT: 3,
+}
