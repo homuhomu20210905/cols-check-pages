5d6b3d468d93b4d8e5a2ffba23f1206c600acbdb
homuhomu20210905 <homuhomu20210905@users.noreply.github.com>
2021-09-22 06:41:44 +0000

    deploy: 0a7841ba8193107cf51d17233b0a66856b4959e3

---
+++ _nuxt/data.a77b87c5.js
@@ -0,0 +1,1 @@
+var s={columns:()=>({cols:1,xl:1,lg:1,md:1,sm:1,xs:1}),setColumns:(s,l)=>(s.cols=l.cols,s.xl=l.xl,s.lg=l.lg,s.md=l.md,s.sm=l.sm,s.xs=l.xs,s),MAX_COUNT:12,NOMAL_COUNT:3};export{s as S};
---
+++ _nuxt/entry-legacy.59a37482.js
@@ -0,0 +1,2 @@
+var imports = ["/cols-check-pages/_nuxt/client-legacy.e50ed2f2.js","/cols-check-pages/_nuxt/data-legacy.28c83673.js"]
+imports.reduce((p, id) => p.then(() => System.import(id)), Promise.resolve())
