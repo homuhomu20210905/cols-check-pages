0c023d95dd6fe2ba55ef09cea7af9a6a0bc9f4a8
homuhomu <gyudouhu3208@gmail.com>
2021-09-23 00:17:19 +0900

    console.log 削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,5 +98,13 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: process.env.NODE_ENV === 'production',
+        },
+      },
+    },
+  },
 }
